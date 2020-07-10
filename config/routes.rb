c9f465c2538c29a1b378cce66b72bdea573b50f3
Meagan Waller <meaganewaller@gmail.com>
2020-07-10 10:19:10 -0400

    Refactor application to be polymorphic

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
-  resources :users, only: [:show, :create] do
+  resources :users, only: [:create] do
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
