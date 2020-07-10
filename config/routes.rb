5547a355572ca7712760a4a5aacc4abf0f2d3cb4
Meagan Waller <meaganewaller@gmail.com>
2020-07-10 16:17:01 -0400

    Fix sign up and add null columns on shout

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, only: [:create]
 
-  resources :users, only: [:create] do
+  resources :users, only: [:create, :show] do
     resource :password,
       controller: "clearance/passwords",
       only: [:create, :edit, :update]
